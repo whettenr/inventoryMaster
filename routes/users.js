9e5b092f5485dc267df2f0c5a71a2ca29c89fe77
mckaycourt <mckay.court@gmail.com>
2018-07-02 14:52:52 -0600

    Fixed adding peripherals and added some log in functionality

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,30 @@
-'use strict';
-
-module.exports = Users;
-
-function Users() {
-    const users = {};
-    let possiblities = ['mmcourt', 'bquinlan'];
-    users.create = async function(netid, password, name, email) {
-
-    };
-
-    users.authenticate = async function(netid) {
-      for(let i in possiblities){
-        if(possiblities[i] === netid){
-          return true;
-        }
-      }
-        return false;
-    };
-
-    users.exists = async function(netid) {
-
-    };
-
-    users.getUser = async function(netid) {
-
-    };
-
-    return users;
-}
+// 'use strict';
+//
+// module.exports = Users;
+//
+// function Users() {
+//     const users = {};
+//     let possiblities = ['mmcourt', 'bquinlan'];
+//     users.create = async function(netid, password, name, email) {
+//
+//     };
+//
+//     users.authenticate = async function(netid) {
+//       for(let i in possiblities){
+//         if(possiblities[i] === netid){
+//           return true;
+//         }
+//       }
+//         return false;
+//     };
+//
+//     users.exists = async function(netid) {
+//
+//     };
+//
+//     users.getUser = async function(netid) {
+//
+//     };
+//
+//     return users;
+// }
