59ca837b2e811efb137c7cff94533e55d2e41eff
mckaycourt <mckay.court@gmail.com>
2018-09-24 16:16:25 -0600

    Added download printers.

--- routes/index.js
+++ routes/index.js
@@ -1482,6 +1482,95 @@ router.get('/download/peripherals', function (req, res, next) {
 
 });
 
+router.get('/download/printers', function (req, res, next) {
+    let Rows = {};
+    let database = new Database(config.getConfig());
+
+    let query = 'SELECT * FROM Printer JOIN Employee on Printer.EmployeeID = Employee.employeeId WHERE Printer.EmployeeID != 400';
+    if (req.query.remove) {
+        let splice = parseInt(req.query.remove);
+        printerFilters.splice(splice, 1);
+
+    }
+    if (req.query.not) {
+        if (printerFilters[req.query.not].includes('!='))
+            printerFilters[req.query.not] = printerFilters[req.query.not].replace('!=', '=');
+        else
+            printerFilters[req.query.not] = printerFilters[req.query.not].replace('=', '!=');
+    }
+    if (req.query.where) {
+        let check = true;
+        for (let i = 0; i < printerFilters.length; i++) {
+            if (printerFilters[i] === req.query.where) {
+                check = false;
+            }
+        }
+        if (check) {
+            printerFilters.push(req.query.where);
+        }
+    }
+    if (printerFilters.length > 0) {
+        query += " and Printer.";
+        for (let filter in printerFilters) {
+            query += printerFilters[filter];
+            query += ' and Printer.';
+            console.log(filter);
+        }
+        query = query.substr(0, query.length - 13);
+    }
+
+    if (req.query.sortby === 'ICN') {
+        query += ' Order BY ICN';
+    }
+    else if (req.query.sortby === 'EmployeeID') {
+        query += ' ORDER BY EmployeeID';
+    }
+    else if (req.query.sortby === 'Make') {
+        query += ' ORDER BY Make';
+    }
+    else if (req.query.sortby === 'Model') {
+        query += ' ORDER BY Model';
+    }
+    else if (req.query.sortby === 'firstName') {
+        query += ' ORDER BY firstName';
+    }
+    else if (req.query.sortby === 'lastName') {
+        query += ' ORDER BY lastName';
+    }
+    else if (req.query.sortby === 'dateAcquired') {
+        query += ' ORDER BY DateAcquired';
+    }
+    else {
+        query += ' Order BY ICN';
+    }
+    console.log(query);
+
+    database.query(query)
+        .then(rows => {
+            Rows = rows;
+            return database.close();
+        })
+        .then(() => {
+            let csvStream = csv.createWriteStream({headers: true}),
+                writableStream = fs.createWriteStream("Printers.csv");
+
+            writableStream.on("finish", function () {
+                console.log("DONE!");
+                let file = __dirname + '/../bin/Printers.csv';
+                console.log(file);
+                res.download(file);
+            });
+
+            csvStream.pipe(writableStream);
+            for (let i = 0; i < Rows.length; i++) {
+                csvStream.write(Rows[i]);
+            }
+            csvStream.end();
+        })
+
+
+});
+
 router.get('/download/monitors', function (req, res, next) {
     let Rows = {};
     let query = 'SELECT * FROM Monitor LEFT JOIN Employee on Monitor.EmployeeID = Employee.employeeId';
